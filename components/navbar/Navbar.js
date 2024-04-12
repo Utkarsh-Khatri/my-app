cdd4fc0d8724885b488e5850bbcd75b265d1c30f
Utkarsh Khatri <utkarshkc7@gmail.com>
2024-04-12 21:40:04 +0545

    Added underline to nav elements and changed color

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,14 +1,15 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar(){
     return (
-        <nav className="flex justify-between bg-sky-400 px-6 py-3">
-            <div>logo</div>
-                <ul className="flex gap-x-6">
-                    <li><Link href='/'>Home</Link></li>
-                    <li><Link href='/about-us'>About Us</Link></li>
-                    <li><Link href='/services'>Services</Link></li>
-                    <li><Link href='/contact'>Contact Us</Link></li>
+        <nav className="flex justify-between items-center bg-sky-400 px-6 py-3">
+                <Image src='/logo.svg' height={60} width={60} className="rounded-full"/>
+                <ul className="flex gap-x-6 text-2xl text-white">
+                    <li className="hover:text-orange-300 hover:underline"><Link href='/'>Home</Link></li>
+                    <li className="hover:text-orange-300 hover:underline"><Link href='/about-us'>About Us</Link></li>
+                    <li className="hover:text-orange-300 hover:underline"><Link href='/services'>Services</Link></li>
+                    <li className="hover:text-orange-300 hover:underline"><Link href='/contact'>Contact Us</Link></li>
                 </ul>
         </nav>
     )
