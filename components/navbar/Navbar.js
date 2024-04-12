920472774787b3a74c1968b73dcb87653d5fa271
Utkarsh Khatri <utkarshkc7@gmail.com>
2024-04-12 21:47:31 +0545

    added color transition in nav items.

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -5,11 +5,11 @@ export default function Navbar(){
     return (
         <nav className="flex justify-between items-center bg-sky-400 px-6 py-3">
                 <Image src='/logo.svg' height={60} width={60} className="rounded-full"/>
-                <ul className="flex gap-x-6 text-2xl text-white">
-                    <li className="hover:text-orange-300 hover:underline"><Link href='/'>Home</Link></li>
-                    <li className="hover:text-orange-300 hover:underline"><Link href='/about-us'>About Us</Link></li>
-                    <li className="hover:text-orange-300 hover:underline"><Link href='/services'>Services</Link></li>
-                    <li className="hover:text-orange-300 hover:underline"><Link href='/contact'>Contact Us</Link></li>
+                <ul className="flex gap-x-6 text-2xl text-white ">
+                    <li className="hover:text-orange-300 transition-colors duration-200"><Link href='/'>Home</Link></li>
+                    <li className="hover:text-orange-300 transition-colors duration-200"><Link href='/about-us'>About Us</Link></li>
+                    <li className="hover:text-orange-300 transition-colors duration-200"><Link href='/services'>Services</Link></li>
+                    <li className="hover:text-orange-300 transition-colors duration-200"><Link href='/contact'>Contact Us</Link></li>
                 </ul>
         </nav>
     )
